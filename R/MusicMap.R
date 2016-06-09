e9f7fd2b99881d16ae89e1a2957f87341cd6cff5
jeffchang5 <chang.jeffrey25@gmail.com>
2016-06-09 11:46:42 +0200

    improved some of the syntax

--- R/MusicMap.R
+++ R/MusicMap.R
@@ -1,18 +1,40 @@
-spotify_auth <- 'https://accounts.spotify.com/api/token'
 
-library('httr')
-# Spotify requires the base64 representation of our client and secret key
+  #####################################
+  ## Spotify requires the base64
+  ## representation of our client
+  ## and secret key
+  #####################################
+
+# Important: References API Keys that weren't pushed to Github.
 
 
-key <- paste0('Basic ', base64(paste0(spotify.client_key, ':', spotify.secret_key)))
+getSpotifyToken <- function() {
+  require(httr)
+  #####################################
+  ## Looks for keys in a subdirectory
+  #####################################
 
-connectToSpotify <- function() {
-  spotify_post <- POST(spotify.url, add_headers(Authorization = key))
-  print(key)
-  print(content(spotify_post, "text"))
+  source('./R/keys/spotify.R')
+  #####################################
+  ## Looks for keys in a subdirectory
+  #####################################
+
+  spotify_auth <- 'https://accounts.spotify.com/api/token'
+  key <- paste0('Basic ', base64(paste0(spotify.client_key, ':', spotify.secret_key)))
+  spotify_post <- POST(spotify.url,
+                       body = list(grant_type = 'client_credentials'),
+                       encoding = 'UTF-8',
+                       encode = 'form',
+                       add_headers("Authorization" = key))
+  spotify_token <<- content(spotify_post)$access_token
+
+
 
 }
 getMusicList <- function() {
-
+  if (exists(spotify_token)) {
+    print("There's something")
+  }
+  else stop("There isn't a Spotify token stored in the environment!")
 }
-connectToSpotify()
+
